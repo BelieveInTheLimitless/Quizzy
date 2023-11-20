e4fcc984e43ded82dfac6fb934a067151b141c3d
Vishal Patil <believeinthelimitless@gmail.com>
2023-11-20 20:03:21 +0530

    Changed data class structure

--- app/src/main/java/com/example/quizzy/model/QuestionItem.kt
+++ app/src/main/java/com/example/quizzy/model/QuestionItem.kt
@@ -1,8 +1,10 @@
 package com.example.quizzy.model
 
 data class QuestionItem(
+    val A: String,
+    val B: String,
+    val C: String,
+    val D: String,
     val answer: String,
-    val category: String,
-    val choices: List<String>,
     val question: String
 )
