34d5061ee45035ed7e3935b7dd8df5f3c6c34dc8
Vishal Patil <believeinthelimitless@gmail.com>
2023-11-20 20:26:22 +0530

    Created AppModule which creates and provides an instance of QuestionAPI

---
+++ app/src/main/java/com/example/quizzy/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.example.quizzy.di
+
+import com.example.quizzy.network.QuestionAPI
+import com.example.quizzy.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideQuestionAPI(): QuestionAPI{
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(QuestionAPI::class.java)
+    }
+}
